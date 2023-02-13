c716f22459298ba9d8ad9ee7ad5cf0a7c00dbb4e
Derek Peacock <derek.peacock@yahoo.com>
2023-02-13 18:27:17 +0000

    removed dbconext temp

--- MVCRoseNoirDatabase/Program.cs
+++ MVCRoseNoirDatabase/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using MVCRoseNoirDatabase.Data;
 
 namespace MVCRoseNoirDatabase
@@ -11,9 +10,10 @@ namespace MVCRoseNoirDatabase
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
